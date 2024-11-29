8fbb4786062f19d02b5778c71f6d5aa85a738053
MJ Polok <mjpolok018@gmail.com>
2024-11-29 22:12:31 +0600

    Set login UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-      App
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+      </Routes>
     </div>
   )
 
