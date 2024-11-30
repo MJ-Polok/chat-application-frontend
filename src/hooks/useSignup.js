c0ed8bf11c6dc39742f05d613a25057e1cacd2c2
MJ Polok <mjpolok018@gmail.com>
2024-12-01 00:43:55 +0600

    SignUp functions setup

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,69 @@
+import { useState } from "react"
+import { useAuthContext } from "../context/AuthContext"
+import toast from "react-hot-toast"
+
+const handleInputErrors = ({ userName, email, password, confirmPassword, gender }) => {
+    if (!userName || !email || !password || !confirmPassword || !gender) {
+        toast.error("Please fill all the fields")
+        return true
+    }
+
+    if (password !== confirmPassword) {
+        toast.error("Password don't matched")
+        return true
+    }
+
+    return false
+}
+
+const useSignup = () => {
+    const [loading, setLoading] = useState(false)
+    const { setAuthUser } = useAuthContext()
+
+    const signup = async ({ userName, email, password, confirmPassword, gender }) => {
+        const checkError = handleInputErrors({
+            userName, email, password, confirmPassword, gender
+        })
+
+        if (checkError) {
+            return
+        }
+
+        try {
+            setLoading(true)
+
+            const res = await fetch("/api/auth/signup", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    userName,
+                    email,
+                    password,
+                    confirmPassword,
+                    gender
+                })
+            })
+
+            const data = await res.json()
+
+            if (data.error) {
+                throw new Error(data.error)
+            }
+
+            // console.log(data);
+            localStorage.setItem('user', JSON.stringify(data))
+            setAuthUser(data)
+
+
+        } catch (error) {
+            toast.error(error.message)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return {loading, signup}
+
+}
+
+export default useSignup
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,30 @@
 import { Link } from "react-router-dom"
 import GenderCheckbox from "../components/GenderCheckbox"
+import { useState } from "react"
+import useSignup from "../hooks/useSignup"
 
 
 const Signup = () => {
+    const [formData, setFormData] = useState({
+        userName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+        gender: ""
+    })
+
+    const {loading, signup} = useSignup()
+
+    const handleCheckboxChange = (gender) => {
+        setFormData({ ...formData, gender })
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+
+        await signup(formData)
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
             <div className="w-full p-6 rounded-lg shadow-md bg-gray-200">
@@ -10,29 +32,39 @@ const Signup = () => {
                     <span className="text-blue-700"> Chat Application</span>
                 </h1>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div>
                         <label className="label p-2">
                             <span className="text-base label-text">Username</span>
                         </label>
-                        <input type="text" placeholder="Enter Your Username" className="w-full input input-bordered h-10" />
+                        <input value={formData.userName} onChange={(e) => setFormData({ ...formData, userName: e.target.value })} type="text" placeholder="Enter Your Username" className="w-full input input-bordered h-10" />
                     </div>
 
                     <div>
                         <label className="label p-2">
                             <span className="text-base label-text">Email</span>
                         </label>
-                        <input type="email" placeholder="Enter Your Email" className="w-full input input-bordered h-10" />
+                        <input value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} type="email" placeholder="Enter Your Email" className="w-full input input-bordered h-10" />
                     </div>
 
                     <div>
                         <label className="label p-2">
                             <span className="text-base label-text">Password</span>
                         </label>
-                        <input type="password" placeholder="Enter Your Password " className="w-full input input-bordered h-10" />
+                        <input value={formData.password} onChange={(e) => setFormData({ ...formData, password: e.target.value })} type="password" placeholder="Enter Your Password " className="w-full input input-bordered h-10" />
                     </div>
 
-                    <GenderCheckbox />
+                    <div>
+                        <label className="label p-2">
+                            <span className="text-base label-text">Password</span>
+                        </label>
+                        <input value={formData.confirmPassword} onChange={(e) => setFormData({ ...formData, confirmPassword: e.target.value })} type="password" placeholder="Again Enter Your Password" className="w-full input input-bordered h-10" />
+                    </div>
+
+                    <GenderCheckbox
+                        onCheckboxChange={handleCheckboxChange}
+                        selectedGender={formData.gender}
+                    />
 
                     <Link to={"/login"} className="text-sm hover:underline hover:text-blue-600 inline-block">
                         {`Already have an account?`}
