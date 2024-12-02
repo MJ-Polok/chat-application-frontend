559d51f46c0cfb41225421bb0b7eb85013b2f5db
MJ Polok <mjpolok018@gmail.com>
2024-12-02 23:30:58 +0600

    Conversations Functions

--- src/components/sidebar/Conversations.jsx
+++ src/components/sidebar/Conversations.jsx
@@ -1,16 +1,20 @@
+import useGetConversation from "../../hooks/useGetConversation"
 import Conversation from "./Conversation"
 
 
 const Conversations = () => {
+    const { loading, conversations } = useGetConversation()
+    // console.log(conversations);
+
     return (
         <div className="py-2 flex flex-col overflow-auto">
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
+            {conversations.map((conversation, index) => (
+                <Conversation key={conversation._id} conversation={conversation} lastIndex={index === conversation.length - 1} />
+            ))}
 
+            {loading ? (
+                <span className="loading loading-spinner"></span>
+            ) : null}
         </div>
     )
 }
